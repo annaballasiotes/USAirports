1e5977b482ae25d860569980b17b0e708a968a1a
= <=>
2019-02-04 11:45:38 -0800

    final

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# USAirports
+# U.S. Airports
+
+This map displays airports in the United States with an enplanement of 25 or more passengers per year. The map distinguishes between airports with and without control towers and emphasizes those airports with greater than 1,000,000 passengers per year. 
+
+Airport data was retrieved from the USGS. The data were derived from an extract of the Public-Use Airports database of the National Transportation Atlas Databases-2010 (NTAD-2010), published by the Bureau of Transportation Statistics, Department of Transportation.
+
+The U.S. state shapefile was aquired from Mike Bostock of D3. Within each state, there is a field counting the number of airports per state. This count is visually displayed in the map and can also be found by clicking on each state.
+
+Acknowledgements: Leaflet, Mike Bostock, USGS, CartoDB, and Bo Zhao.
--- js/main.js
+++ js/main.js
@@ -9,58 +9,47 @@ var mymap = L.map('map', {
 // 2. Add a base map.
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
 
-// 3. Add cell towers GeoJSON Data
-// Null variable that will hold cell tower data
+// 3. Add airports GeoJSON Data
 var airports = null;
-
 
 // 4. build up a set of colors from colorbrewer's dark2 category
 var colors = chroma.scale('Paired').mode('lch').colors(3);
 
 // 5. dynamically append style classes to this page. This style classes will be used for colorize the markers.
 for (i = 0; i < 9; i++) {
-    $('head').append($("<style> .marker-color-" + (i + 1).toString() + " { color: " + colors[i] + "; font-size: 15px; text-shadow: 0 0 3px #ffffff;} </style>"));
+    $('head').append($("<style> .marker-color-" + (i + 1).toString() +
+    " { color: " + colors[i] + "; font-size: 10px; text-shadow: 0 0 3px #ffffff;} </style>"));
 }
 
 // Get GeoJSON and put on it on the map when it loads
 airports = L.geoJson.ajax("assets/airports.geojson", {
-    // assign a function to the onEachFeature parameter of the cellTowers object.
-    // Then each (point) feature will bind a popup window.
-    // The content of the popup window is the value of `feature.properties.company`
     onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.AIRPT_NAME);
+        layer.bindPopup("<b>Airport: </b>" + feature.properties.AIRPT_NAME +
+        "<br>Total enplanement (boarded passengers) per year: " + feature.properties.TOT_ENP);
     },
     pointToLayer: function (feature, latlng) {
         var id = 0;
-        if (feature.properties.TOT_ENP > 1000000) {id = 2; }
+        if (feature.properties.TOT_ENP > 1000000) return L.marker(latlng, {icon: L.divIcon({className: 'fas fa-plane marker-color-3'.toString() })})
         else if (feature.properties.CNTL_TWR == "Y") { id = 1; }
-        else { id = 0;} //
-        return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-circle marker-color-' + (id + 1).toString() })});
+          return L.marker(latlng, {icon: L.divIcon({className: 'fas fa-circle marker-color-' + (id + 1).toString() })})
     },
-    attribution: 'Control Tower Data &copy; Map Cruzin | Oregon counties &copy; Oregon Explorer | Base Map &copy; CartoDB | Made By Anna Ballasiotes'
+    attribution: 'Airport Data &copy; USGS | State Boundaries &copy; Mike Bostock | Base Map &copy; CartoDB | Made By Anna Ballasiotes'
 }).addTo(mymap);
 
-
+//State Chloropleth
 var states = null;
-states = L.geoJson.ajax("assets/us-states.geojson", {
-    style: style
-}).addTo(mymap);
-
-// 6. Set function for color ramp
-colors = chroma.scale('Purples').colors(5); //colors = chroma.scale('RdPu').colors(5);
+colors = chroma.scale('Oranges').colors(5);
 
 function setColor(count) {
     var id = 0;
-    if (count > 18) { id = 4; }
-    else if (count > 13 && count <= 18) { id = 3; }
-    else if (count > 10 && count <= 13) { id = 2; }
+    if (count > 20) { id = 4; }
+    else if (count > 15 && count <= 20) { id = 3; }
+    else if (count > 10 && count <= 15) { id = 2; }
     else if (count > 5 &&  count <= 10) { id = 1; }
     else  { id = 0; }
     return colors[id];
 }
 
-
-// 7. Set style function that sets fill color.md property equal to cell tower density
 function style(feature) {
     return {
         fillColor: setColor(feature.properties.count),
@@ -72,10 +61,12 @@ function style(feature) {
     };
 }
 
-// 8. Add state polygons
-// create state variable, and assign null to it.
-
-
+states = L.geoJson.ajax("assets/us-states.geojson", {
+    style: style,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("There are " + feature.properties.count + " airports in " + feature.properties.name + ".");
+      }
+}).addTo(mymap);
 
 // 9. Create Leaflet Control Object for Legend
 var legend = L.control({position: 'topright'});
@@ -85,16 +76,16 @@ legend.onAdd = function () {
 
     // Create Div Element and Populate it with HTML
     var div = L.DomUtil.create('div', 'legend');
-    div.innerHTML += '<b>Airport Density per State</b><br />';
-    div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>19+</p>';
-    div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>14-18</p>';
-    div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>11-13</p>';
+    div.innerHTML += '<b>Airports per State</b><br />';
+    div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>21+</p>';
+    div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>16-20</p>';
+    div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>11-15</p>';
     div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 6-10</p>';
     div.innerHTML += '<i style="background: ' + colors[0] + '; opacity: 0.5"></i><p> 0- 5</p>';
     div.innerHTML += '<hr><b>Airports<b><br />';
-    div.innerHTML += '<i class="fa fa-plane marker-color-1"></i><p> No Control Tower</p>';
-    div.innerHTML += '<i class="fa fa-plane marker-color-2"></i><p> With Control Tower</p>';
-    div.innerHTML += '<i class="fa fa-plane marker-color-3"></i><p> Control Tower & greater than 1,000,000 ENP</p>';
+    div.innerHTML += '<i class="fa fa-circle marker-color-1"></i><p>With No Control Tower</p>';
+    div.innerHTML += '<i class="fa fa-circle marker-color-2"></i><p> With Control Tower</p>';
+    div.innerHTML += '<i class="fas fa-plane marker-color-3"></i><p> With Control Tower & greater than 1,000,000 passengers</p>';
     // Return the Legend div containing the HTML content
     return div;
 };
